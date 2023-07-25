3249b793ff599d00a708aec7335e4c0763739c41
Kamonrat65 <136230822+Kamonrat65@users.noreply.github.com>
2023-07-25 17:24:07 +0700

    react-create app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <>
+      <div className="card">
+        <Avatar />
+        <div className="data">
+          <Intro />
+          <SkillList />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="draw a picture" emoji="🎨🧹" color="orange" />
+      <Skill skill="play a game" emoji="🎮" color="skyblue" />
+      <Skill skill="python" emoji="✊" color="yellow" />
+      <Skill skill="javascript" emoji="🤏" color="lime" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>นางสาวกมลรัตน์ สาลีวรรณ</h1>
+      <p>
+        นักศึกษา ICT อารมณ์ดี เฟรนลี่ แต่โลกส่วนตัวสูง
+        จากสาขาเทคโนโลยีสารสนเทศและการสื่อสาร คณะวิทยาศาสตร์
+        มหาวิทยาลัยอุบลราชธานี ชื่นชอบการวาดภาพและออกแบบ
+        อยากเป็นนักพัฒนาเว็บโปรแกรมมิ่งที่เก่งในอนาคต😇
+      </p>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="kamon.jpg" alt="My Avatar"></img>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
